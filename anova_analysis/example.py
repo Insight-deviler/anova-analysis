f0ff716b19ae634e63b242fefe0a0ac1e9e61d26
Sarath S <insightagri10@gmail.com>
2023-06-03 08:41:25 +0530

    Add files via upload

--- anova_analysis/example.py
+++ anova_analysis/example.py
@@ -5,4 +5,18 @@ treat = 23
 file = "data/MODEL_DATA.xlsx"
 output = 'MODEL_DATA'
 
-ANOVA_RBD.RBD(rep, treat, file, output)
+result = ANOVA_RBD.RBD(rep, treat, file, output)
+
+CF = result["correction_factor"]
+TSS = result["total_sum_of_square"]
+RSS = result["replication_sum_of_square"]
+TSS = result["treatment_sum_of_square"]
+ESS = result["error_sum_of_square"]
+Rdf = result["replication_df"]
+Tdf = result["treatment_df"],
+Edf = result["errors_df"],
+RMSS = result["rep_mean_ss"],
+TMSS = result["tre_mean_ss"],
+EMSS = result["error_mean_ss"]
+
+print("")
