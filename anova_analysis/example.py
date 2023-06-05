81887cf01796c4c210819ada4439c083398943bd
Sarath S <insightagri10@gmail.com>
2023-06-05 22:11:43 +0530

    Update example.py

--- anova_analysis/example.py
+++ anova_analysis/example.py
@@ -2,6 +2,8 @@ from anova_analysis import ANOVA_RBD
 
 rep = 4
 treat = 23
+
+# If you have a single excel sheet for single character
 file = "data/MODEL_DATA.xlsx"
 output = 'MODEL_DATA'
 
@@ -20,3 +22,26 @@ TMSS = result["tre_mean_ss"],
 EMSS = result["error_mean_ss"]
 
 print("")
+
+# If you have multiple characters in separate excel sheet
+# See this GitHub page for further details "https://github.com/Insight-deviler/Folder-based-Character-Column-Transformation/tree/main"
+
+from anova_analysis import ANOVA_RBD
+import os
+
+folder_path = r'C:/Users/PlantReading/data/'
+
+# listing files in the folder_path 
+for file in os.listdir(folder_path):
+  
+# Checking whether the folder contains excel files
+    if file.endswith('.xlsx') or file.endswith('.xls'):
+
+# Joining the file path and name of the excel file:
+        file_path = os.path.join(folder_path, file)
+        print(f"processing file: {file}")
+       
+      # Where: 
+      #   rep is replication,
+      #   treat is genotypes,  os.path.basename(file) for getting file name
+        ANOVA_RBD.RBD(rep,treat,file_path,os.path.basename(file))
