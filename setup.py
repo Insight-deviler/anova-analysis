e20b10c4d4bb894454564bd6f07fadf36fd6014d
Sarath S <insightagri10@gmail.com>
2023-06-27 23:44:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # Setting up
 setup(
         name="anova_analysis", 
-        version="0.0.1.1",
+        version="0.0.1.2",
         authors=['Sarath S', 'Saranyadevi S'],
         author_email="insightagri10@gmail.com",
         description="A package for performing various ANOVA analysis",
         license='MIT',
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        packages=find_packages(),
+        packages=['ANOVA_RBD', 'ANOVA_CRD'],
+        package_dir={'': 'src'},
         url="https://github.com/Insight-deviler/anova_analysis",
-        install_requires=["tabulate", "pandas", "scipy",'numpy','math','warnings'],
+        install_requires=["tabulate", "pandas", "scipy",'numpy','warnings'],
         keywords=['Genetics and Plant breeding','Quantitative Genetics', 'ANOVA', 'Two-way ANOVA', 'RBD', 'CRD', 'One-Way ANOVA', 'analysis of variance'],
         classifiers= [
              "Programming Language :: Python :: 3",
